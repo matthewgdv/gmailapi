b15f842767d0c49ae41f6bc6af52efd04d247520
matthewgdv <matthewgdv@gmail.com>
2019-11-14 23:20:19 +0000

    Add requirements.txt and bump version number

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 from os import path
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    dependencies = [line for line in f if line]
 
 setup(
     name="gmailapi",
@@ -22,15 +25,7 @@ setup(
       "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        "google-api-python-client",
-        "google-auth",
-        "pymaybe",
-        "pathmagic",
-        "pysubtypes",
-        "pymiscutils",
-        "pyiotools",
-    ],
+    install_requires=dependencies,
     setup_requires=['setuptools_scm'],
     include_package_data=True,
     author="Matt GdV",
